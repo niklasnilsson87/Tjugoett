06902598fb455dae5460b989323b962bd6d05204
Niklas Nilsson <niklas.nilsson1987@gmail.com>
2019-05-14 12:40:33 +0200

    Initializing a console application

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nn222ia_examination_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
